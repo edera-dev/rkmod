c278d89eb2af5e99fdfefaf84d8e54ca513015f4
Alex Zenla <alex@edera.dev>
2025-08-16 14:58:53 -0700

    current example: show loaded modules

--- examples/current.rs
+++ examples/current.rs
@@ -1,12 +1,24 @@
 use rkmod::cache::InternCache;
+use rkmod::controller::SystemModuleController;
 use rkmod::directory::ModuleDirectory;
 
 fn main() {
     let directory =
         ModuleDirectory::current(InternCache::default()).expect("could not load module directory");
+    let controller = SystemModuleController::default();
+    let mut loaded: usize = 0;
+    for module in directory.database().modules().values() {
+        let is_live = controller.is_live(module.name().as_str()).unwrap_or(false);
+
+        if is_live {
+            loaded += 1;
+        }
+    }
+
     println!(
-        "{} has {} modules",
+        "{} has {} modules ({} loaded)",
         directory.root().to_string_lossy(),
-        directory.database().modules().len()
+        directory.database().modules().len(),
+        loaded,
     );
 }
