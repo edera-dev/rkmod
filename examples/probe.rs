beaf62a789cc85842342b42bf9b88e58b31c5960
Alex Zenla <alex@edera.dev>
2025-08-16 14:52:34 -0700

    probe example: take modules from arguments

--- examples/probe.rs
+++ examples/probe.rs
@@ -3,12 +3,17 @@ use rkmod::controller::SystemModuleController;
 use rkmod::directory::ModuleDirectory;
 use rkmod::manager::ModuleManager;
 use rkmod::util::normalize_module_name;
+use std::sync::Arc;
 
 fn main() {
     let cache = InternCache::default();
+    let modules: Vec<Arc<String>> = std::env::args()
+        .skip(1)
+        .map(|module| cache.get_string(normalize_module_name(module)))
+        .collect();
+
     let directory =
         ModuleDirectory::current(cache.clone()).expect("failed to load module directory");
     let manager = ModuleManager::new(directory, SystemModuleController::default());
-    let module = cache.get_string(normalize_module_name("batman-adv"));
-    manager.probe(&[module]).expect("failed to probe module");
+    manager.probe(&modules).expect("failed to probe modules");
 }
